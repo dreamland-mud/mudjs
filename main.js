004abd96e9ed9e95d59de8cc033eb1d42d6b9224
Filths Satyr <filths.satyr@gmail.com>
2018-05-15 11:33:43 +1200

    Check protocol version

--- main.js
+++ main.js
@@ -1,45 +1,54 @@
 
+var PROTO_VERSION = 'DreamLand Web Client/1.1';
 var msgs = [];
 var send = function() {}, notify = function() {};
 
 $(document).ready(function() {
-    var telnet = new Telnet();
-
-    var handlers = {
-        'console-out': function(b) {
-            telnet.process(b);
-        },
-        'notify': function(b) {
-            notify(b);
-        },
-        'alert': function(b) {
-            alert(b);
-        },
-        'prompt': function(b) {
-            $('#stats').show();
-            
-            function stat($node, value, max) {
-                $node.find('.fill').css({ width: (100*value/max) + '%' });
-                $node.find('.value').text(value + ' / ' + max);
-            }
-
-            stat($('#stats .hit'), b.hit, b.max_hit);
-            stat($('#stats .mana'), b.mana, b.max_mana);
-            stat($('#stats .move'), b.move, b.max_move);
-        }
-    };
 
     function process(s) {
         $('#terminal').trigger('output', [s]);
         $('#input input').focus();
     }
 
-    telnet.handleRaw = function(s) {
-        process(s);
-    }
-
     function connect() {
         ws = new WebSocket(wsUrl, ['binary']);
+
+        var telnet = new Telnet();
+
+        telnet.handleRaw = function(s) {
+            process(s);
+        }
+
+        var handlers = {
+            'console-out': function(b) {
+                telnet.process(b);
+            },
+            'notify': function(b) {
+                notify(b);
+            },
+            'alert': function(b) {
+                alert(b);
+            },
+            'prompt': function(b) {
+                $('#stats').show();
+                
+                function stat($node, value, max) {
+                    $node.find('.fill').css({ width: (100*value/max) + '%' });
+                    $node.find('.value').text(value + ' / ' + max);
+                }
+
+                stat($('#stats .hit'), b.hit, b.max_hit);
+                stat($('#stats .mana'), b.mana, b.max_mana);
+                stat($('#stats .move'), b.move, b.max_move);
+            },
+            'version': function(b) {
+                if(b !== PROTO_VERSION) {
+                    process('\n\u001b[1;31mВерсия клиента (' + PROTO_VERSION + ') не совпадает с версией сервера (' + b + ').\n' +
+                            'Обнови страницу, если не поможет - почисти кеши.\n');
+                    ws.close();
+                }
+            }
+        };
 
         ws.binaryType = 'arraybuffer';
         ws.onmessage = function(e) {
