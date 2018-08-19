d8eb762c706cd16e75d62793a7fc02f3c6b4a174
Ruffina Koza <ruffina.koza@gmail.com>
2018-08-19 14:25:37 +1200

    Add support for looking at extra descr by clicking on it.

--- manip.js
+++ manip.js
@@ -2,8 +2,22 @@ var manipParseAndReplace;
 
 $(document).ready(function() {
 
-    // Replace item manipulation "<m i='234234' c='take $,put $ 12348'/>" tags surrounding every item.
     manipParseAndReplace = function(span) {
+        // Replace "<r i='sign'>sign</r>" tags surrounding extra descriptions.
+        span.find('r').each(function(index) {
+            var id = $(this).attr('i');
+            var keyword = $(this).contents();
+
+            $(this).replaceWith(function() {
+                var result = $('<span/>')
+                    .addClass('manip-ed')
+                    .attr('data-id', id)
+                    .append(keyword);
+                return result;
+            });
+        });
+
+        // Replace item manipulation "<m i='234234' c='take $,put $ 12348'/>" tags surrounding every item.
         span.find('m').each(function(index) {
             // Populate menu node for each item based on the 'c' and 'l' attributes containing command lists.
             var id = $(this).attr('i');
@@ -45,6 +59,12 @@ $(document).ready(function() {
             });
         });
     };
+
+    // Send 'read' command to the server when extra descr keyword is clicked.
+    $('body').on('click', '.manip-ed', function(e) {
+        var id = $(e.currentTarget).attr('data-id');
+        send("read '" + id + "'");
+    });
 
     // Send command to the server when individual menu item is clicked.
     $('body').on('click', '.manip-item', function(e) {
