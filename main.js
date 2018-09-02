33db7e959bb9dbc4fc138d7a6ae820a348bdd4af
Ruffina Koza <ruffina.koza@gmail.com>
2018-09-02 18:07:59 +1200

    Move keypad buttons handlers to main.js.

--- main.js
+++ main.js
@@ -182,5 +182,67 @@ $(document).ready(function() {
                 }
             });
     }
+
+    /*
+     * Handlers for plus-minus buttons to change terminal font size.
+     */ 
+    var fontDelta = 2;
+    
+    function changeFontSize(delta) {
+        var style = terminal.css('font-size'); 
+        var fontSize = parseFloat(style); 
+        terminal.css('font-size', (fontSize + delta) + 'px');
+    }
+
+    $('#font-plus-button').click(function(e) {
+        e.preventDefault();
+        changeFontSize(fontDelta);
+    });
+
+    $('#font-minus-button').click(function(e) {
+        e.preventDefault();
+        changeFontSize(-fontDelta);
+    });
+
+    /*
+     * Handlers for 'keypad' key area.
+     */
+    // Long press: open/close direction etc.
+    var btnTimer;
+    var wasLongPress = false;
+    var longPressDelay = 800;
+
+    $('.btn-keypad').on('touchstart', function(e) {
+        wasLongPress = false;
+
+        // Send specified long-cmd once the delay has elapsed.
+        btnTimer = setTimeout(function() {
+            btnTimer = null;
+            wasLongPress = true;
+            var btn = $(e.currentTarget), cmd = btn.data('long-cmd');
+            if (cmd) {
+                send(cmd);
+            }
+
+        }, longPressDelay);
+
+    }).on('touchend', function(e) {
+        if (btnTimer)  
+            clearTimeout(btnTimer);
+    });
+
+    // Single click: go direction, look etc.`
+    $('.btn-keypad').click(function(e) {
+        if (wasLongPress)
+            return;
+
+        e.preventDefault();
+        var btn = $(e.currentTarget), cmd = btn.data('cmd');
+
+        if (cmd) {
+            send(cmd);
+        }
+    });
+
 });
 
