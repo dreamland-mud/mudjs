876ead716e85746b541154a8a121881d1958b58a
Ruffina Koza <ruffina.koza@gmail.com>
2019-09-16 11:49:54 +1200

    Show input placeholder hints for newbie zones.

--- src/location.js
+++ src/location.js
@@ -1,5 +1,6 @@
 
 var sessionId = require('./sessionid')();
+var placeholders = require('./data/placeholders.json');
 
 var lastLocation, locationChannel;
 
@@ -25,12 +26,52 @@ function bcastLocation(loc) {
     }
 };
 
+/** 
+  Choose a placeholder text for the main command input. Placeholders are 
+  kept in a json file, per each area and room vnum or generic ones (*).
+  Room placeholders can be an array of hint commands, or an entire hint string.
+ */
+function createPlaceholder(loc) {
+    if (!placeholders)
+        return '';
+
+    var areahint = placeholders[loc.area] || placeholders["*"];
+    if (!areahint) 
+        return '';
+   
+    var roomhints = areahint[loc.vnum] || areahint["*"];
+    if (!roomhints) 
+        return '';
+
+    if (typeof roomhints === 'string')
+        return roomhints;
+
+    if (Array.isArray(roomhints)) {
+        var index;
+
+        if (roomhints.length == 0)
+            return ''; 
+        
+        // When just entered a new room, show the first hint as the 'main' one.
+        if (!lastLocation || loc.vnum !== lastLocation.vnum)
+            index = 0;
+        else
+            index = Math.floor(Math.random() * roomhints.length);
+    
+        return 'Введи любую команду, например: ' + roomhints[index];        
+    }
+
+    return '';
+}
+
 $(document).ready(function() {
     $('#rpc-events').on('rpc-prompt', function(e, b) {
-        bcastLocation({
+        var loc = {
             area: b.area,
             vnum: b.vnum
-        });
+        };
+        $("#input input").attr("placeholder", createPlaceholder(loc));
+        bcastLocation(loc);
     });
 });
 
