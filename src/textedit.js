711e1bdd6fab8e9c5d4b8f047c57ed5fa687280b
Ruffina Koza <ruffina.koza@gmail.com>
2019-12-01 20:34:11 +1300

    Retrieve help ids on each editor open.

--- src/textedit.js
+++ src/textedit.js
@@ -4,6 +4,35 @@ require('brace/theme/monokai');
 
 var websock = require('./websock');
 
+// Help keyword and id lookup inside webedit (if you run 'webedit help'):
+function initHelpIds() {
+    var heditLookup = $('#textedit-modal input');
+
+    $.get("/help/hedit.json", function(data) { 
+        console.log('Retrieved', data.length, 'help ids.');
+        
+        var topics = $.map(data, function(dataItem) { 
+            return {value: dataItem.id + ': ' + dataItem.kw.toLowerCase(), 
+                    data: dataItem.id}; 
+        });
+        
+        heditLookup.autocomplete({ 
+            lookup: topics, 
+            lookupLimit: 20, 
+            autoSelectFirst: true, 
+            showNoSuggestionNotice: true, 
+            noSuggestionNotice: 'Справка не найдена', 
+            onSelect: function(suggestion) { 
+                $('#textedit-modal .editor').focus();
+            } 
+        }); 
+
+    }, 'json').fail(function() {
+        console.log('Cannot retrieve help ids.');
+        $('#textedit-modal input').hide();
+    }); 
+}
+
 $(document).ready(function() {
     var editor = ace.edit($('#textedit-modal .editor')[0]);
 
@@ -13,8 +42,10 @@ $(document).ready(function() {
         editor.setValue(text);
         $('#textedit-modal').modal('show');
 
-        if (arg === 'help')
+        if (arg === 'help') {
             $('#textedit-modal input').show();
+            initHelpIds();
+        }
         else
             $('#textedit-modal input').hide();
 
