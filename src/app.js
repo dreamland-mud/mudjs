1df4fd0a312e4f3bbd716330e84555c87330a0d3
Filths <filths.satyr@gmail.com>
2020-02-24 21:23:56 +1300

    Remove AppBar

--- src/app.js
+++ src/app.js
@@ -39,28 +39,14 @@ const useStyles = makeStyles(theme => ({
 
 export default props => {
     // Hooks
-    const [panel, setPanel] = useState(true);
     const classes = useStyles();
     const bigScreen = useMediaQuery(theme => theme.breakpoints.up('sm'));
 
-    const toggle = e => setPanel(!panel);
-
-    const appBar = bigScreen &&
-        <AppBar position="static" color="default">
-            <Toolbar variant="dense">
-                <Typography className={classes.title}>I'm sure Ruffina will find some very important text to put in here.</Typography>
-                <IconButton edge="end" onClick={toggle} className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-            </Toolbar>
-        </AppBar>;
-
     return <Box display="flex" flexDirection="column" className={classes.page}>
-        { appBar }
         <Box flex="1 1 auto" className={classes.main}>
             <SplitterLayout secondaryInitialSize={270} secondaryMinSize={270}>
                 <Terminal />
-                { panel && bigScreen && <Panel /> }
+                { bigScreen && <Panel /> }
             </SplitterLayout>
         </Box>
         <button id="reconnect" type="button" className="btn btn-primary">Reconnect</button>
