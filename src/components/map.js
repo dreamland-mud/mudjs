f74d2cc93e05c13fb52e4c18837508c959382f1e
Ruffina Koza <ruffina.koza@gmail.com>
2020-10-21 21:21:45 +1300

    Add zoom in/out button for the map, with cached values.

--- src/components/map.js
+++ src/components/map.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import { makeStyles } from "@material-ui/core/styles";
+import { Add, Remove } from "@material-ui/icons";
 import $ from 'jquery';
 import 'arrive';
 var lastLocation = require('../location');
+
+const mapFontSizeKey = "map-font-size";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    zIndex: 500
+  }
+}));
+
+const MapControls = props => {
+    const classes = useStyles();
+
+    const changeFontSize = delta => {
+        var map = $('#map');
+        var style = map.css('font-size'); 
+        var fontSize = parseFloat(style); 
+        map.css('font-size', (fontSize + delta) + 'px');
+        localStorage.setItem(mapFontSizeKey, fontSize + delta);
+    };
+
+    return (
+        <div className={classes.root}>
+          <ButtonGroup variant="contained" size="small" orientation="vertical" color="primary" aria-label="map resize buttons">
+              <Button onClick={() => { changeFontSize(1) }}> <Add /> </Button>
+              <Button onClick={() => { changeFontSize(-1) }}> <Remove /> </Button>
+          </ButtonGroup>
+        </div>
+      );
+};
 
 export default class Map extends React.Component {
     prevArea = ''
@@ -54,6 +90,13 @@ export default class Map extends React.Component {
         this.refreshMap(loc.area, loc.vnum);
     }
 
+    initMapFontSize() {
+        let cacheFontSize = localStorage.getItem(mapFontSizeKey);
+        if (cacheFontSize != null) {
+            $('#map').css('font-size', (cacheFontSize) + 'px');
+        }
+    }
+
     refreshMap(area, room) {
         let mapName = area.replace(/are$/, 'html');
         let mapUrl = `/maps/sources/${mapName}`;
@@ -74,11 +117,15 @@ export default class Map extends React.Component {
     componentDidMount() {
         this.initLocationChannel();
         this.initMap();
+        this.initMapFontSize();
     }
 
     render() {
-        return <div id="map-wrap">
+        return <>
+                <MapControls/>
+                <div id="map-wrap">
                     <div id="map"></div>
-                </div>;
+                </div>
+              </>;
     }
 }
