4a4c60f6a8dd8976b15aafb739f9bb445b09c21f
Ruffina Koza <ruffina.koza@gmail.com>
2020-10-22 14:46:04 +1300

    Add top bar with area name; recenter map after font size change.

--- src/components/map.js
+++ src/components/map.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from "@material-ui/core/styles";
 import { Add, Remove } from "@material-ui/icons";
 import $ from 'jquery';
 import 'arrive';
 var lastLocation = require('../location');
+
+const areas = require('../data/areas.json').reduce(function(map, obj) {
+    map[obj.file] = obj.name;
+    return map;
+}, {});
 
 const mapFontSizeKey = "map-font-size";
 
@@ -15,46 +24,77 @@ const useStyles = makeStyles((theme) => ({
     bottom: 20,
     right: 20,
     zIndex: 500
+  },
+  title: {
+    flexGrow: 1
+  },
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 500
   }
+
 }));
 
-const MapControls = props => {
-    const classes = useStyles();
-
-    const changeFontSize = delta => {
-        var map = $('#map');
-        var style = map.css('font-size'); 
-        var fontSize = parseFloat(style); 
-        map.css('font-size', (fontSize + delta) + 'px');
-        localStorage.setItem(mapFontSizeKey, fontSize + delta);
-    };
-
-    return (
-        <div className={classes.root}>
-          <ButtonGroup variant="contained" size="small" orientation="vertical" color="primary" aria-label="map resize buttons">
-              <Button onClick={() => { changeFontSize(1) }}> <Add /> </Button>
-              <Button onClick={() => { changeFontSize(-1) }}> <Remove /> </Button>
-          </ButtonGroup>
-        </div>
-      );
-};
-
 export default class Map extends React.Component {
+    AppBar = props => {
+        const classes = useStyles();
+
+        return (<AppBar className={classes.appbar} color="default">
+                <Toolbar variant="dense">
+                    <Typography id="areaName" className={classes.title}></Typography>
+                </Toolbar>
+            </AppBar>
+        );
+    };
+    
+    MapControls = props => {
+        let mapComponent = this;
+        const classes = useStyles();
+    
+        const changeFontSize = delta => {
+            var map = $('#map');
+            var style = map.css('font-size'); 
+            var fontSize = parseFloat(style); 
+            map.css('font-size', (fontSize + delta) + 'px');
+            localStorage.setItem(mapFontSizeKey, fontSize + delta);
+
+            // TODO remember if the room was in view before font change, otherwise don't change position.
+            mapComponent.recenterPosition();
+        };
+    
+        return (
+            <div className={classes.root}>
+              <ButtonGroup variant="contained" size="small" orientation="vertical" color="primary" aria-label="map resize buttons">
+                  <Button onClick={() => { changeFontSize(1) }}> <Add /> </Button>
+                  <Button onClick={() => { changeFontSize(-1) }}> <Remove /> </Button>
+              </ButtonGroup>
+            </div>
+          );
+    };
+    
+    
     prevArea = ''
 
     clearMap() {
         $('#map').html("");
+        $('#areaName').html("");
+    }
+
+    recenterPosition() {
+        var $active = $('#map .room.active');
+        if (!$active.length)
+            return;
+
+        $active.get(0).scrollIntoView({block: 'center', inline: 'center'});
     }
 
     highlightPosition(currentRoom) {
         $('#map .room').removeClass('active');
         $('#map .room-' + currentRoom).addClass('active');
 
-        var $active = $('#map .room.active');
-        if (!$active.length)
-            return;
-
-        $active.get(0).scrollIntoView({block: 'center', inline: 'center'});
+        this.recenterPosition();
     }
 
     initLocationChannel() {
@@ -100,6 +140,7 @@ export default class Map extends React.Component {
     refreshMap(area, room) {
         let mapName = area.replace(/are$/, 'html');
         let mapUrl = `/maps/sources/${mapName}`;
+        let areaName = areas[area] || '';
 
         $.get(mapUrl)
             .then(data => {
@@ -108,6 +149,9 @@ export default class Map extends React.Component {
                 document.arrive('.room-' + room, {onceOnly: true, existing: true}, () => {
                     this.highlightPosition(room);
                 });
+
+                $('#areaName').html(areaName);
+
             }).catch(e => {
                 console.log('Map error', e);
                 this.clearMap();
@@ -122,7 +166,8 @@ export default class Map extends React.Component {
 
     render() {
         return <>
-                <MapControls/>
+                <this.AppBar/>
+                <this.MapControls/>
                 <div id="map-wrap">
                     <div id="map"></div>
                 </div>
