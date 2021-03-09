02ace56818efa20c29bc43bdbc0698b1768110e9
Targorn <nv.einhorn@gmail.com>
2021-03-09 16:20:30 +0000

    Added buttons for work with history for mobiles

--- src/components/cmdinput.js
+++ src/components/cmdinput.js
@@ -5,13 +5,35 @@ import ReactDom from 'react-dom';
 import { useSelector } from 'react-redux';
 import $ from 'jquery';
 import { echo } from '../input';
+import { send } from '../websock';
 import settings from '../settings';
 import { connect } from '../websock';
+import { makeStyles } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const scrollPage = dir => {
     const wrap = $('.terminal-wrap');
     wrap.scrollTop(wrap.scrollTop() + wrap.height()*dir);
 };
+
+const useStyles = makeStyles(theme => ({
+    activity: {
+        // display: 'flex',
+        flexDirection: 'row'
+    },
+    btn: {
+        pointerEvents: 'all',
+        height: "1.7em",
+        width: "1.7em"
+    },
+    history: {
+        position: 'absolute',
+        // alignItems: 'flex-end',
+        bottom: 0,
+        right: 0
+    }
+}));
+
 
 const input_history = localStorage.history ? JSON.parse(localStorage.history) : [];
 let position = input_history.length;
@@ -20,6 +42,8 @@ let current_cmd = '';
 // This component draws either a "Reconnect" button or the main input field for the terminal.
 // Handles command history browsing and terminal scrolling (pgUp/pgDown).
 const CmdInput = props => {
+    const classes = useStyles();
+    const big = useMediaQuery(theme => theme.breakpoints.up('sm'));
     // A hook to access the redux store's state; takes a selector function as an argument. 
     // The selector is called with the store state. Component's render will be called only when
     // 'connection' field has changed.
@@ -29,6 +53,29 @@ const CmdInput = props => {
 
     // Arrow up. Retrieve previous command from history and populate the input box.
     // Called from onKeyDown handler.
+
+    function saveCmd(t) {
+        if(t) {
+            position = input_history.length;
+            if (input_history.length === 0 || t !== input_history[input_history.length-1]) {
+                    input_history[position++] = t;
+
+                    let drop = input_history.length - 1000; // store only 1000 most recent entries;
+                
+                    if(drop < 0)
+                        drop = 0;
+                
+                    const save = JSON.stringify(input_history.slice(drop));
+                
+                    try {
+                        localStorage.history = save;
+                    } catch(e) {
+                        console.log('Opps, saving command history to the local storage: save.length=' + save.length, e);
+                    }
+            }
+	    }
+    }
+
     const historyUp = () => {
         if(position > 0) {
             if(position == input_history.length)
@@ -60,6 +107,19 @@ const CmdInput = props => {
             setValue(current_cmd);
         }
     };
+
+    const historyRepeat = () => {
+        if (!input_history[position]) {
+            var cmd = input_history[position-1]
+        } else {
+            var cmd = input_history[position]
+        }
+        setValue('');
+        position = input_history.length;
+        saveCmd(cmd)
+        echo(cmd)
+        send(cmd)
+    }
 
     // Main onKeyDown handler. Reacts to pageUp/pageDown to scroll the main terminal window,
     // arrow keys to navigate history, and passes everything else to the user-defined triggers (settings).
@@ -97,27 +157,8 @@ const CmdInput = props => {
         e.preventDefault();
         const t = value;
         setValue('');
-
-        if(t) {
-            position = input_history.length;
-	    if (input_history.length === 0 || t !== input_history[input_history.length-1]) {
-                input_history[position++] = t;
-
-                let drop = input_history.length - 1000; // store only 1000 most recent entries;
-            
-                if(drop < 0)
-                    drop = 0;
-            
-                const save = JSON.stringify(input_history.slice(drop));
-            
-                try {
-                    localStorage.history = save;
-                } catch(e) {
-                    console.log('Opps, saving command history to the local storage: save.length=' + save.length, e);
-                }
-            }
-	}
-
+        //Check if we must to store command
+        saveCmd(t)
         // For each input line, trigger the 'input' event. Default 'input' handler will send the command
         // to the server, and also user-defined triggers will be called.
         var lines = t.split('\n');
@@ -131,9 +172,22 @@ const CmdInput = props => {
     // onChange ensures that the state of this component changes whenever user inputs something.
     // Current 'value' state is used from all key handlers.
     return connection.connected ?
-            <form onSubmit={submit} id="input">
-                <input id="inputBox" onKeyDown={keydown} value={value} onChange={e => setValue(e.target.value)} type="text" autoComplete="off" />
-            </form> :
+            <div flex="1" className={classes.activity}>
+                <form onSubmit={submit} id="input">
+                    <input id="inputBox" onKeyDown={keydown} value={value} onChange={e => setValue(e.target.value)} type="text" autoComplete="off" />
+                </form> 
+                { !big > 0 &&
+                <table className={classes.history}>
+                    <tbody>
+                        <tr>
+                            <button onClick={historyRepeat} cmd="repeat" className={`btn btn-sm btn-ctrl btn-outline-primary ${classes.btn}`}><i className="fa fa-repeat"></i></button>
+                            <button onClick={historyDown} cmd="history-down" className={`btn btn-sm btn-ctrl btn-outline-primary ${classes.btn}`}><i className="fa fa-arrow-down"></i></button>
+                            <button onClick={historyUp} cmd="history-up" className={`btn btn-sm btn-ctrl btn-outline-primary ${classes.btn}`}><i className="fa fa-arrow-up"></i></button>		    
+                        </tr>
+                    </tbody>
+                </table>
+                }
+            </div> :
             <button onClick={connect} type="button" className="btn btn-primary">Reconnect</button>;
 };
 
